8c638236fb349aabb28ab8e312db544e6ea0cea3
marcialwushu <marcialwushu@gmail.com>
2018-03-18 19:52:16 -0300

    getDBMeter funcion added - test implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { DBMeter } from '@ionic-native/db-meter';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public subscription = new Array<any>();
+
+  constructor(
+  	public navCtrl: NavController,
+  	private dbMeter : DBMeter
+  ) {
 
   }
+
+  getDBMeter(){
+  	// Start listening
+	this.subscription = this.dbMeter.start().subscribe(
+  		data => console.log(data)
+	);
+
+  }
+
+
+
 
 }
