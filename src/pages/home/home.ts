ae8fcc7a3fb7b43199b00971872595d5cb179868
marcialwushu <marcialwushu@gmail.com>
2018-03-18 21:03:34 -0300

    db-meter functions native plugin added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DBMeter } from '@ionic-native/db-meter';
- 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-	public subscription = new Array<any>();
+	public subscription;
 
   constructor(
   	public navCtrl: NavController,
@@ -17,14 +17,33 @@ export class HomePage {
 
   }
 
-  getDBMeter(){
+  startDBMeter(){
   	// Start listening
-	this.subscription = this.dbMeter.start().subscribe(
+	  this.subscription = this.dbMeter.start().subscribe(
   		data => console.log(data)
 	);
 
   }
 
+  stopDBMeter(){
+    // Stop listening
+    this.subscription.unsubscribe();
+  }
+
+  listingDBMeter(){
+    // Check if we are listening
+    this.dbMeter.isListening().then(
+      (isListening: boolean) => console.log(isListening)
+    );
+  }
+
+  deleteDBMeter(){
+    // Delete DBMeter instance from memory
+    this.dbMeter.delete().then(
+      () => console.log('Deleted DB Meter instance'),
+      error => console.log('Error occurred while deleting DB Meter instance')
+    );
+  }
 
 
 
